bce5ef3f5fd0ad832a3911315d06c41514789c57
Cara Eppes <ceppes@udel.edu>
2019-04-24 15:24:46 -0400

    initial commit

---
+++ src/main/java/com/caraeppes/lemurapi/LemurApiApplication.java
@@ -0,0 +1,13 @@
+package com.caraeppes.lemurapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemurApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemurApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caraeppes/lemurapi/LemurApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.caraeppes.lemurapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LemurApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
